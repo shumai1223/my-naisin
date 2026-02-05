f5bf5336a11fb891f807ef84bd3e150252041741
shumai1223 <shuyankunii@gmail.com>
2026-02-06 00:37:46 +0900

    Add files via upload

--- src/app/reverse/page.tsx
+++ src/app/reverse/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
+export const revalidate = false;
 
 import * as React from 'react';
 import Link from 'next/link';
