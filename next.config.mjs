d0c8dd7adbfa5a9c78a26f5d550900a19d1adfb2
shumai1223 <shuyankunii@gmail.com>
2026-02-08 11:54:28 +0900

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/blog/suisen-vs-ippan-naishin',
+        destination: '/blog/naishin-guide#制度編',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
