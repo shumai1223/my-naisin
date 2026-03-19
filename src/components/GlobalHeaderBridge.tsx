d5e3fce9d8ce71315e0e51f1df51d8a8edd8e98e
shumai1223 <shuyankunii@gmail.com>
2026-03-19 19:59:22 +0900

    Add files via upload

---
+++ src/components/GlobalHeaderBridge.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { Header } from "@/components/Header";
+
+export function GlobalHeaderBridge() {
+  const pathname = usePathname();
+
+  if (pathname === "/") {
+    return null;
+  }
+
+  return <Header />;
+}
